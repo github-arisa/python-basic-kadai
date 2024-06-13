f92adbb8f049afceb1c346a487fc42412a99d23d
github-arisa <als.tkhs.prg@gmail.com>
2024-06-13 12:56:07 +0900

    Update kadai_005.py

--- kadai_005.py
+++ kadai_005.py
@@ -4,10 +4,14 @@
 
 #公式：台形の面積 =（上辺＋下辺）× 高さ ÷ 2
 
+#変数を定義
 upper_base = 10
 lower_base = 20
 height = 5
 
 area = (upper_base + lower_base) * height / 2
-print(area)
 
+#出力
+#\u00B2はUnicode文字
+# Pythonの文字列で上付き文字（スーパースクリプト）を表現したい場合使用
+print(f"{area} cm\u00B2")
